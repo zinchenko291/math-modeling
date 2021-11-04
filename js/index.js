47db49303f745ee6cb0b65fe50f2a08b6f8d1004
zinchenko291 <zinchenko291@gmail.com>
2021-11-04 18:11:31 +0500

    Сделал основную страницу

---
+++ js/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let colors = {
+    green: "#54A48B",
+    yellow: "#c0a761"
+};
+
+let lessonsBtn = document.querySelector("#lessonsBtn");
+let worksBtn = document.querySelector("#worksBtn");
+
+let lessonsList = document.querySelector("#lessonsList");
+let worksList = document.querySelector("#worksList");
+let header = document.querySelector("header");
+
+lessonsBtn.addEventListener("click", (e) => {
+    lessonsBtn.classList.add("active");
+    worksBtn.classList.remove("active");
+
+    lessonsList.classList.remove("hidden");
+    worksList.classList.add("hidden");
+    header.style = "background-color: " + colors.green;
+})
+
+worksBtn.addEventListener("click", (e) => {
+    lessonsBtn.classList.remove("active");
+    worksBtn.classList.add("active");
+
+    lessonsList.classList.add("hidden");
+    worksList.classList.remove("hidden");
+    header.style = "background-color: " + colors.yellow;
+})
